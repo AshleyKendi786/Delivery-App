af8acd8ff6208968f1594de382464050cf9ae913
saidabdulhakim48@gmail.com <saidabdulhakim48@gmail.com>
2025-09-23 18:16:40 +0300

    Modified some changes

--- backend/models.py
+++ backend/models.py
@@ -24,7 +24,7 @@ class Order(db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key = True)
     customer_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable = False)
-    product_id = db.Column(db.Integer, db.ForeignKey("products.id") nullable = False)
+    product_id = db.Column(db.Integer, db.ForeignKey("products.id"), nullable = False)
     address = db.Column(db.String(100), nullable = False)
     price = db.column(db.Float, nullable = False)
     status = db.Column(db.String, default = "pending") # pending, start delivery, delivered
